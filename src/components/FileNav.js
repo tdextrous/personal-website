1626ef0dbbb6de2d8fbd42786d5668ef431c0aa6
Thomas Dexter <t.dextrous@gmail.com>
2019-09-01 18:27:12 -0500

    remove extra nav links

--- src/components/FileNav.js
+++ src/components/FileNav.js
@@ -10,18 +10,13 @@ const FileNav = ({ active, handleClick }) => {
                 <h2 className="file-nav__title">Site Directory</h2>
             </div>
             <div className="file-nav__link-container">
-                <FileLink text="home" url="/" handleClick={handleClick} />
                 <FileFolder text="projects">
                     <FileLink text="mountainStatesElectrical" url="/projects/msec" depth={1} handleClick={handleClick} />
                     <FileLink text="veazee" url="/projects/veazee" depth={1} handleClick={handleClick} />
                     <FileLink text="github" url="/projects/github" depth={1} handleClick={handleClick} />
                     <FileLink text="codepen" url="/projects/codepen" depth={1} handleClick={handleClick} />
                 </FileFolder>
-                <FileFolder text="externalLinks">
-                    <FileLink text="github" depth={1} handleClick={handleClick} />
-                    <FileLink text="codepen" depth={1} handleClick={handleClick} />
-                    <FileLink text="linkedIn" depth={1} handleClick={handleClick} />
-                </FileFolder>
+                <FileLink text="home" url="/" handleClick={handleClick} />
                 <FileLink text="meta" url="/meta" handleClick={handleClick} />
             </div>
         </div>
