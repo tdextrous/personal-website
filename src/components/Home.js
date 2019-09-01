97b8c17634289b65af0c5654b768ed16b293bf20
Thomas Dexter <t.dextrous@gmail.com>
2019-08-31 23:36:44 -0500

    Minor text edits on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,8 @@ const Home = () => (
                 Thomas Dexter
             </h1>
             <ul className="home__list">
-                <li className="home__list-item">student @ <a href="#" className="link">texas a&amp;m university</a></li>
-                <li className="home__list-item">aspiring web developer</li>
+                <li className="home__list-item">freshman electrical engineering student @ texas a&amp;m university</li>
+                <li className="home__list-item">amateur web developer</li>
                 <li className="home__list-item">currently seeking internship for summer 2020</li>
             </ul>
         </div>
