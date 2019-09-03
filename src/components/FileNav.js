bd4d7cd1fa04c396ebb1260a27ed9f80d7103c0f
Thomas Dexter <t.dextrous@gmail.com>
2019-09-03 00:19:50 -0500

    Remove github and codepen project page links, not ready yet

--- src/components/FileNav.js
+++ src/components/FileNav.js
@@ -13,8 +13,6 @@ const FileNav = ({ active, handleClick }) => {
                 <FileFolder text="projects">
                     <FileLink text="mountainStatesElectrical" url="/projects/msec" depth={1} handleClick={handleClick} />
                     <FileLink text="veazee" url="/projects/veazee" depth={1} handleClick={handleClick} />
-                    <FileLink text="github" url="/projects/github" depth={1} handleClick={handleClick} />
-                    <FileLink text="codepen" url="/projects/codepen" depth={1} handleClick={handleClick} />
                 </FileFolder>
                 <FileLink text="home" url="/" handleClick={handleClick} />
                 <FileLink text="meta" url="/meta" handleClick={handleClick} />
